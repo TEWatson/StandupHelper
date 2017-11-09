81aaa7e3760f3458528d182f0bfb08b3d9c6d9ff
Taylor Watson <watstayl@amazon.com>
2017-11-08 21:34:39 -0800

    Minor

--- SesLambdaSrc/index.js
+++ SesLambdaSrc/index.js
@@ -7,6 +7,8 @@ exports.handler = function(event, context, callback) {
     var sesNotification = event.Records[0].ses;
     var sender = sesNotification.mail.source;
     var content = sesNotification.content
+    console.log(sender)
+    console.log(content)
     standupUpdatesTable
         .update({hash: sender, range: content })
         .then(function(resp) {
